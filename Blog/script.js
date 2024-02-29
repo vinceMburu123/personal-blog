ff9f4d46a2335b31b81a1587148a589ba82f6626
vinceMburu123 <128956764+vinceMburu123@users.noreply.github.com>
2024-02-29 15:03:26 +0300

    Add files via upload

---
+++ Blog/script.js
@@ -0,0 +1,11 @@
+// Add smooth scroll functionality to navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const sectionId = this.getAttribute('href').substring(1);
+      document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
